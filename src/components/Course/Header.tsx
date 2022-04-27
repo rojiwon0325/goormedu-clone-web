f830804c29c799c22c8cec1f0ce2b8ee761369f4
rojiwon0325 <bond9986@naver.com>
2022-04-27 16:35:48 +0900

    complete count bugfix

--- src/components/Course/Header.tsx
+++ src/components/Course/Header.tsx
@@ -87,8 +87,8 @@ const LearnRecordInfo: React.FC<{ courseId: number }> = ({ courseId }) => {
         <div className="w-full">
           <div className="h-5 text-sm">
             강의 진행도
-            {learnRecord?.count_completion_record ??
-              0 / (courseLectureList[courseId]?.length || 1)}
+            {(learnRecord?.count_completion_record ?? 0) /
+              (courseLectureList[courseId]?.length || 1)}
             %
           </div>
           <div className="h-5 w-full bg-blue rounded-lg overflow-hidden relative">
